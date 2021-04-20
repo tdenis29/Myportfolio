0cf4b31adfad041337d59639ea4c91eb452261cc
tdenis29 <tdenis29@outlook.com>
2021-04-20 11:01:58 -0700

    scrollspy+modalform

--- js/app.js
+++ js/app.js
@@ -1,22 +1,3 @@
-// Get the modal
-let modal = document.getElementById("myModal");
-// Get button
-let btn = document.getElementById("email");
-// get close
-let span = document.getElementsByClassName("btn-close")[0];
-
-var myModal = new bootstrap.Modal(document.getElementById('myModal'), {
-    keyboard: false
-  })
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }}
+var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+  target: '#nav'
+})
